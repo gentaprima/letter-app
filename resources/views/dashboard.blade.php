41d1db2b0e7c93d59cea64491e3f6c49a66a6e66
prasetya <prasetya2423@gmail.com>
2022-06-26 16:10:35 +0700

    fix waka role

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -10,30 +10,28 @@
         <section class="content">
             <div class="container-fluid">
                 <div class="row">
-                    @if (session('users')['role'] == 0 || session('users')['role'] == 4)
-                        <div class="col-xl-4 col-sm-7 mb-xl-0 mb-4">
-                            <div class="card">
-                                <div class="card-body p-3">
-                                    <div class="row">
-                                        <div class="col-8">
-                                            <div class="numbers">
-                                                <p class="text-sm mb-0 text-capitalize font-weight-bold">Jumlah Surat Masuk
-                                                </p>
-                                                <span
-                                                    class="font-weight-bolder mb-0 text-success text-sm font-weight-bolder">{{ $letterIn }}</span>
-                                            </div>
+                    <div class="col-xl-4 col-sm-7 mb-xl-0 mb-4">
+                        <div class="card">
+                            <div class="card-body p-3">
+                                <div class="row">
+                                    <div class="col-8">
+                                        <div class="numbers">
+                                            <p class="text-sm mb-0 text-capitalize font-weight-bold">Jumlah Surat Masuk
+                                            </p>
+                                            <span
+                                                class="font-weight-bolder mb-0 text-success text-sm font-weight-bolder">{{ $letterIn }}</span>
                                         </div>
-                                        <div class="col-4 text-end">
-                                            <div
-                                                class="icon icon-shape bg-gradient-primary shadow text-center border-radius-md">
-                                                <i class="ni ni-money-coins text-lg opacity-10" aria-hidden="true"></i>
-                                            </div>
+                                    </div>
+                                    <div class="col-4 text-end">
+                                        <div
+                                            class="icon icon-shape bg-gradient-primary shadow text-center border-radius-md">
+                                            <i class="ni ni-money-coins text-lg opacity-10" aria-hidden="true"></i>
                                         </div>
                                     </div>
                                 </div>
                             </div>
                         </div>
-                    @endif
+                    </div>
                     <!-- ./col -->
                     @if (session('users')['role'] == 0 || session('users')['role'] == 4)
                         <div class="col-xl-4 col-sm-7 mb-xl-0 mb-4">
@@ -61,29 +59,31 @@
                     @endif
                     <!-- ./col -->
                     <!-- ./col -->
-                    <div class="col-xl-4 col-sm-7 mb-xl-0 mb-4">
-                        <div class="card">
-                            <div class="card-body p-3">
-                                <div class="row">
-                                    <div class="col-8">
-                                        <div class="numbers">
-                                            <p class="text-sm mb-0 text-capitalize font-weight-bold">Jumlah Instansi</p>
-                                            <h5 class="font-weight-bolder mb-0">
-                                                <span
-                                                    class="font-weight-bolder mb-0 text-success text-sm font-weight-bolder">{{ $instance }}</span>
-                                            </h5>
+                    @if (session('users')['role'] == 0 || session('users')['role'] == 4)
+                        <div class="col-xl-4 col-sm-7 mb-xl-0 mb-4">
+                            <div class="card">
+                                <div class="card-body p-3">
+                                    <div class="row">
+                                        <div class="col-8">
+                                            <div class="numbers">
+                                                <p class="text-sm mb-0 text-capitalize font-weight-bold">Jumlah Instansi</p>
+                                                <h5 class="font-weight-bolder mb-0">
+                                                    <span
+                                                        class="font-weight-bolder mb-0 text-success text-sm font-weight-bolder">{{ $instance }}</span>
+                                                </h5>
+                                            </div>
                                         </div>
-                                    </div>
-                                    <div class="col-4 text-end">
-                                        <div
-                                            class="icon icon-shape bg-gradient-primary shadow text-center border-radius-md">
-                                            <i class="ni ni-money-coins text-lg opacity-10" aria-hidden="true"></i>
+                                        <div class="col-4 text-end">
+                                            <div
+                                                class="icon icon-shape bg-gradient-primary shadow text-center border-radius-md">
+                                                <i class="ni ni-money-coins text-lg opacity-10" aria-hidden="true"></i>
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
                             </div>
                         </div>
-                    </div>
+                    @endif
                 </div>
                 {{-- <div class="alert alert-primary mt-5 text-white" role="alert">
               Ada Surat Masuk Baru Yang Didisposisikan Kepada mu. <a class="text-success text-bold" href="">Lihat Disini</a>
