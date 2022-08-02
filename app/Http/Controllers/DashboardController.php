6991e63f528ed57bb7004ffe3c43a084b3c7a736
Prasetya Hadi Saputra <phs@192.168.0.111>
2022-08-02 22:14:28 +0700

    fix terespon by users id

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -6,11 +6,13 @@ use App\Models\EvaluationLetter;
 use App\Models\ModelInstance;
 use App\Models\ModelLetter;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Session;
 
 class DashboardController extends Controller
 {
     public function index()
     {
+
         $data['letterOut'] = ModelLetter::where('type', '1');
         if (session("users")['role'] !== 0 && session("users")['role'] !== 4) {
             $data['letterOut'] = $data['letterOut']->where('id_users', session("users")['id']);
@@ -19,12 +21,11 @@ class DashboardController extends Controller
         if (session("users")['role'] !== 0 && session("users")['role'] !== 4) {
             $data['letterIn'] = $data['letterIn']->where('id_users', session("users")['id']);
         }
-        $data['countResponse'] = EvaluationLetter::where("response", '!=', null)->count();
+        $data['countResponse'] = EvaluationLetter::where("response", '!=', null)->leftJoin('surat', 'tindak_lanjut.id_surat', '=', 'surat.id')->where('surat.id_users', '=', Session::get('users')->id)->count();
         $data['letterOut'] = $data['letterOut']->count();
         $data['letterIn'] = $data['letterIn']->count();
         $data['dispos'] = EvaluationLetter::where("is_approve", '=', 1)->distinct()->count("id_surat");
-        $data['outLetterApprove'] = DB::table("surat")->where("type",'=','1')->where("is_out_letter_approve",'1')->get()->count();
-        // dd($data['outLetterApprove']);
+        $data['outLetterApprove'] = DB::table("surat")->where("type", '=', '1')->where("is_out_letter_approve", '1')->get()->count();
 
         return view("dashboard", $data);
     }
